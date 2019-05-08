d6f57110d4b38465e0d3552d63194d8b0c3e3bde
Chin Hong Lim <clim0028@student.monash.edu>
2019-05-08 21:34:29 +1000

    Added NPC abstract class

---
+++ src/game/NPC.java
@@ -0,0 +1,43 @@
+package game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.monash.fit2099.engine.*;
+
+public abstract class NPC extends Actor{
+
+	public NPC(String name) {
+		super(name, 'Q', 5, 50);
+	}
+	
+	protected List<ActionFactory> actionFactories = new ArrayList<ActionFactory>();
+	
+	protected void addBehaviour(ActionFactory behaviour) {
+		actionFactories.add(behaviour);
+	}
+	
+	@Override
+	public Action playTurn(Actions actions, GameMap map, Display display) {
+		// NPC will do its behaviors before action
+		for (ActionFactory factory : actionFactories) {
+			Action action = factory.getAction(this, map);
+			if(action != null)
+				return action;
+		}
+		
+		// if no behavior are done, it skips its turn
+		for (Action action : actions) {
+			if (action instanceof SkipTurnAction) {
+				return action;
+			}
+		}
+		return null;
+	}
+	
+	@Override
+	public Actions getAllowableActions(Actor otherActor, String direction, GameMap map) {
+		return new Actions();
+	}
+
+}
