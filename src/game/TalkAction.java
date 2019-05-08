e0677ed279656762ed9f443b5d2f87be8d73eb79
Chin Hong Lim <clim0028@student.monash.edu>
2019-05-08 21:12:40 +1000

    Added TalkAction

---
+++ src/game/TalkAction.java
@@ -0,0 +1,37 @@
+package game;
+
+import java.util.List;
+
+import edu.monash.fit2099.engine.*;
+
+public class TalkAction extends Action{
+	Actor subject;
+	
+	public TalkAction(Actor subject) {
+		this.subject = subject;
+	}
+
+	@Override
+	public String execute(Actor actor, GameMap map) {
+		List<Item> items = actor.getInventory();
+		
+		for (Item item : items) {
+			if (item.toString().equals("Rocket Plan")) {
+				return "Hand them over, I don't have all day!";
+			}
+		}
+		
+		return "I can give you something that will help, but I'm going to need the plans.";
+	}
+
+	@Override
+	public String menuDescription(Actor actor) {
+		return actor + " talks to " + subject;
+	}
+
+	@Override
+	public String hotKey() {
+		return "";
+	}
+
+}
