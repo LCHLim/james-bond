8911d27f38431fa2910fc879e9d1403f8ad3ad53
Chin Hong Lim <clim0028@student.monash.edu>
2019-05-08 20:56:50 +1000

    Complete BuildRocketAction and linked it to RocketPad

--- src/game/BuildRocketAction.java
+++ src/game/BuildRocketAction.java
@@ -1,19 +1,43 @@
 package game;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.Item;
+import edu.monash.fit2099.engine.Location;
 
 public class BuildRocketAction extends Action{
-
+	Location here;
+	
+	public BuildRocketAction(Location location) {
+		here = location;
+	}
+	
 	@Override
 	public String execute(Actor actor, GameMap map) {
-		return actor + "has successfully built the rocket !";
+		List<Item> items = here.getItems();
+		ArrayList<Item> toBeRemovedItems = new ArrayList<Item> ();
+		
+		for (Item item : items) {
+			String itemName = item.toString();
+			if (itemName.equals("Rocket Engine") || itemName.equals("Rocket Body")) {
+				toBeRemovedItems.add(item);
+			}
+		}
+		
+		for (Item item : toBeRemovedItems) {
+			here.removeItem(item);
+		}
+		
+		return actor + " has successfully built the rocket !";
 	}
 
 	@Override
 	public String menuDescription(Actor actor) {
-		return actor + "builds rocket";
+		return actor + " builds rocket";
 	}
 
 	@Override
--- src/game/RocketPad.java
+++ src/game/RocketPad.java
@@ -1,12 +1,33 @@
 package game;
 
-import edu.monash.fit2099.engine.Actor;
-import edu.monash.fit2099.engine.Ground;
+import java.util.*;
+import edu.monash.fit2099.engine.*;
 
 public class RocketPad extends Ground{
 
 	public RocketPad() {
 		super('R');
+	}
+	
+	@Override
+	public Actions allowableActions(Actor actor, Location location, String direction){
+		List<Item> items = location.getItems();
+		int requiredItemCount = 2;
+	
+		for (Item item : items) {
+			String itemName = item.toString();
+			
+			if (itemName.equals("Rocket Engine") || itemName.equals("Rocket Body")) {
+				requiredItemCount--;
+			}
+		}
+		
+		
+		if (actor instanceof Player && requiredItemCount== 0) {
+			return new Actions(new BuildRocketAction(location));
+		}
+		
+		return new Actions();
 	}
 
 }
