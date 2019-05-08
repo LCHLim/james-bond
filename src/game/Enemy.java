936ef3500eac6778525f0e7c5625f2d62bd4d260
Chin Hong Lim <clim0028@student.monash.edu>
2019-05-08 16:13:16 +1000

    added enemy abstract class to the system

---
+++ src/game/Enemy.java
@@ -0,0 +1,52 @@
+package game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.monash.fit2099.engine.*;
+
+public abstract class Enemy extends Actor{
+
+	public Enemy(String name, char displayChar) {
+		// this is enemies base priority and hitpoints
+		// can be used as a reference for other type of enemy
+		super(name, displayChar, 5, 50);
+	}
+	
+	private List<ActionFactory> actionFactories = new ArrayList<ActionFactory>();
+	
+	private void addBehaviour(ActionFactory behaviour) {
+		actionFactories.add(behaviour);
+	}
+	
+	@Override
+	public Action playTurn(Actions actions, GameMap map, Display display) {
+		// enemy will do its behaviors before action
+		for (ActionFactory factory : actionFactories) {
+			Action action = factory.getAction(this, map);
+			if(action != null)
+				return action;
+		}
+		
+		// if no behavior are done, either attack or skip turn
+		for (Action action : actions) {
+			if (action instanceof AttackAction) {
+				return action;
+			}
+			if (action instanceof SkipTurnAction) {
+				return action;
+			}
+		}
+		return null;
+	}
+	
+	@Override
+	public Actions getAllowableActions(Actor otherActor, String direction, GameMap map) {
+		// only player can attack enemy, enemy can't attack each other
+		if (otherActor instanceof Player) {
+			return new Actions(new AttackAction(otherActor, this));
+		}
+		return new Actions();
+	}
+
+}
