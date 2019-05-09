dddd3174d28d3dd1e445cb7bb2a99d4cf5ca786c
Yashodha Liyanage Don <yliy0002@student.monash.edu>
2019-05-10 02:22:40 +1000

    Documented StunnablePlayer using Javadoc

--- src/game/StunnablePlayer.java
+++ src/game/StunnablePlayer.java
@@ -6,23 +6,47 @@ import edu.monash.fit2099.engine.Display;
 import edu.monash.fit2099.engine.Player;
 import edu.monash.fit2099.engine.SkipTurnAction;
 
+/**
+ * Class representing the player that could be stunned by Ninja
+ */
 public class StunnablePlayer extends Player{
 
 	private boolean isStunned = false;
 	int stunCounter = 0;
 	
+	/**
+	 * Super constructor for StunnablePlayer class extended from the Player class.
+	 * 
+	 * @param name name of the player
+	 * @param displayChar character to represent the player in the UI
+	 * @param priority how early in the turn the player can act
+	 * @param hitPoints player's starting number of hitpoints
+	 */
 	public StunnablePlayer(String name, char displayChar, int priority, int hitPoints) {
 		super(name, displayChar, priority, hitPoints);
 	}
 	
+	/**
+	 * Mutates the isStunned field of the Player class.
+	 * 
+	 * @param b boolean value of true/false
+	 */
 	public void setIsStunned(boolean b) {
 		isStunned = b;
 	}
 	
+	/**
+	 * Returns the value of the isStunned field.
+	 * 
+	 * @return boolean value of true/false
+	 */
 	public boolean getIsStunned() {
 		return isStunned;
 	}
 	
+	/**
+	 * Overridden method from the parent class to make the player stunnable.
+	 */
 	@Override
 	protected Action showMenu(Actions actions, Display display) {
 		if (isStunned && stunCounter >= 2) {
