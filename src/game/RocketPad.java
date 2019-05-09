f28fcc521d7a1106e654e54e871ea1a3a6c72054
Yashodha Liyanage Don <yliy0002@student.monash.edu>
2019-05-09 18:36:50 +1000

    Added Javadoc documentation to RocketPad

--- src/game/RocketPad.java
+++ src/game/RocketPad.java
@@ -2,12 +2,30 @@ package game;
 
 import edu.monash.fit2099.engine.*;
 
+/**
+ * Class representing the rocket pad.
+ */
 public class RocketPad extends Ground{
-
+	
+	/**
+	 * Super constructor of the RocketPad class extended from the Ground class.
+	 */
 	public RocketPad() {
 		super('R');
 	}
 	
+	/**
+	 * Overridden method from the parent class to implement allowable actions on RocketPad.
+	 * <P>
+	 * An instance of BuildRocketAction is created as an allowable action on RocketPad if the Actor considered
+	 * is an instance of Player.
+	 * <P>
+	 * Returns an empty Action list otherwise.
+	 * 
+	 * @param actor the Actor acting
+	 * @param location the current Location
+	 * @param direction the direction of the RocketPad from the Actor
+	 */
 	@Override
 	public Actions allowableActions(Actor actor, Location location, String direction){
 		if (actor instanceof Player) {
