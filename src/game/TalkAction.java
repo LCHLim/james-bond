006151fef6d644eed1cee58d970906c6352eed99
Yashodha Liyanage Don <yliy0002@student.monash.edu>
2019-05-10 03:53:24 +1000

    Documented TalkAction using Javadoc

--- src/game/TalkAction.java
+++ src/game/TalkAction.java
@@ -4,13 +4,27 @@ import java.util.List;
 
 import edu.monash.fit2099.engine.*;
 
+/**
+ * Class representing the talk action of the player.
+ */
 public class TalkAction extends Action{
 	Actor subject;
 	
+	/**
+	 * Constructor for the TalkAction class, which in an extension of the Action class.
+	 * 
+	 * @param subject actor on whom the talk action is performed on
+	 */
 	public TalkAction(Actor subject) {
 		this.subject = subject;
 	}
 
+	/**
+	 * Overridden method from the parent class to execute talk action.
+	 * <P>
+	 * Returns strings when the player talks with or without the plan in hand
+	 * specifically.
+	 */
 	@Override
 	public String execute(Actor actor, GameMap map) {
 		List<Item> items = actor.getInventory();
@@ -24,11 +38,19 @@ public class TalkAction extends Action{
 		return subject + ": I can give you something that will help, but I'm going to need the plans.";
 	}
 
+	/**
+	 * Overridden method from the parent class to get a string returned when
+	 * the player talks to the desired party.
+	 */
 	@Override
 	public String menuDescription(Actor actor) {
 		return actor + " talks to " + subject;
 	}
 
+	/**
+	 * Overridden method from the parent class to return the key used in the menu
+	 * for triggering the action of talking.
+	 */
 	@Override
 	public String hotKey() {
 		return "";
