37333e3af7346c2550014c43790069577cb1ce95
Yashodha Liyanage Don <yliy0002@student.monash.edu>
2019-05-09 20:36:31 +1000

    Added Javadoc documentation to Goon

--- src/game/Goon.java
+++ src/game/Goon.java
@@ -4,17 +4,32 @@ import edu.monash.fit2099.engine.*;
 
 import java.util.Random;
 
+/**
+ * Class representing the enemy Goon.
+ */
 public class Goon extends Enemy{
 	private Random rand = new Random();
 	
+	/**
+	 * Super constructor of the Goon extended from the Enemy class.
+	 * <P>
+	 * The method also adds an instance of FollowBehaviour to Goon.
+	 * 
+	 * @param name name of the Goon
+	 * @param player player
+	 */
 	public Goon(String name, Actor player) {
 		super(name, 'G');
 		addBehaviour(new FollowBehaviour(player));
 	}
 	
+	/**
+	 * Overridden method from parent class to change enemy's random action as a
+	 * non-player character.
+	 */
 	@Override
 	public Action playTurn(Actions actions, GameMap map, Display display) {
-		// enemy will do its behaviors before action
+		// enemy will do its behaviours before action
 		for (ActionFactory factory : actionFactories) {
 			Action action = factory.getAction(this, map);
 			if(action != null)
@@ -26,7 +41,7 @@ public class Goon extends Enemy{
 			return new InsultAction(this);
 		}
 		
-		// if no behavior are done, either attack or skip turn
+		// if no behaviour are done, either attack or skip turn
 		for (Action action : actions) {
 			if (action instanceof AttackAction) {
 				return action;
@@ -38,6 +53,10 @@ public class Goon extends Enemy{
 		return null;
 	}
 	
+	/**
+	 * Overridden method from the parent class to modify enemy's default damage to be
+	 * twice as such.
+	 */
 	@Override
 	protected IntrinsicWeapon getIntrinsicWeapon() {
 		int baseDamage = super.getIntrinsicWeapon().damage();
