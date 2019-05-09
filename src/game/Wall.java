6513ef8c75be82a5c8d8a521883cab2678dfb450
Yashodha Liyanage Don <yliy0002@student.monash.edu>
2019-05-09 17:50:47 +1000

    Documented Floor and Wall classes using Javadoc

--- src/game/Wall.java
+++ src/game/Wall.java
@@ -3,17 +3,29 @@ package game;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.Ground;
 
+/**
+ * Class representing the walls on the map.
+ */
 public class Wall extends Ground {
-
+	
+	/**
+	 * Super constructor of the Wall class extended from the Ground class.
+	 */
 	public Wall() {
 		super('#');
 	}
 	
+	/**
+	 * Overridden the method the from parent class to implement Wall to be impassable terrain.
+	 */
 	@Override
 	public boolean canActorEnter(Actor actor) {
 		return false;
 	}
 	
+	/**
+	 * Overridden the method from the parent class to implement Wall to be terrain that blocks thrown objects.
+	 */
 	@Override
 	public boolean blocksThrownObjects() {
 		return true;
