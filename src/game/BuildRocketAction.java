1d103ad83d5362fe9a1c9238f7f7293eec50afa3
Yashodha Liyanage Don <yliy0002@student.monash.edu>
2019-05-10 04:02:45 +1000

    Documented BuildRocketAction using Javadoc

--- src/game/BuildRocketAction.java
+++ src/game/BuildRocketAction.java
@@ -9,13 +9,29 @@ import edu.monash.fit2099.engine.GameMap;
 import edu.monash.fit2099.engine.Item;
 import edu.monash.fit2099.engine.Location;
 
+/**
+ * Class representing the rocket building action of the player.
+ */
 public class BuildRocketAction extends Action{
 	Location here;
 	
+	/**
+	 * Constructor for the BuildRocketAction class, which in an extension of the Action class.
+	 * 
+	 * @param location location at which the rocket is to be built
+	 */
 	public BuildRocketAction(Location location) {
 		here = location;
 	}
 	
+	/**
+	 * Overridden method from the parent class to execute rocket build action if
+	 * the player has eligible items in the inventory, ie. a rocket engine and
+	 * a rocket body.
+	 * <P>
+	 * Returns strings when the player build the rocket with or without the engine
+	 * and the body in hand specifically.
+	 */
 	@Override
 	public String execute(Actor actor, GameMap map) {
 		List<Item> items = here.getItems();
@@ -43,11 +59,19 @@ public class BuildRocketAction extends Action{
 		}
 	}
 
+	/**
+	 * Overridden method from the parent class to get a string returned when
+	 * the player builds the rocket.
+	 */
 	@Override
 	public String menuDescription(Actor actor) {
 		return actor + " builds rocket";
 	}
 
+	/**
+	 * Overridden method from the parent class to return the key used in the menu
+	 * for triggering the action of building a rocket.
+	 */
 	@Override
 	public String hotKey() {
 		return "";
