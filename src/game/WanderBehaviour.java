f033ddfb4844d4d5899f7c0059d0bf4bea774ae2
Yashodha Liyanage Don <yliy0002@student.monash.edu>
2019-05-10 04:18:28 +1000

    Added Javadoc documentation to WanderBehaviour

--- src/game/WanderBehaviour.java
+++ src/game/WanderBehaviour.java
@@ -7,15 +7,24 @@ import java.util.concurrent.ArrayBlockingQueue;
 
 import edu.monash.fit2099.engine.*;
 
-
+/**
+ * Class representing wander behaviour of actors.
+ */
 public class WanderBehaviour implements ActionFactory{
 
 	Random rand = new Random();
 	
+	/**
+	 * Constructor for the FollowBehaviour class, which is implemented from the
+	 * ActionFactory interface.
+	 */
 	public WanderBehaviour() {
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Overridden method from the interface ActionFactory.
+	 */
 	@Override
 	public Action getAction(Actor actor, GameMap map) {
 		Location here = map.locationOf(actor);
