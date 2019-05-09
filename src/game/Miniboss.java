52bd83479aae245d1ab018372b023fd68744935e
Yashodha Liyanage Don <yliy0002@student.monash.edu>
2019-05-09 20:40:42 +1000

    Documented Miniboss with Javadoc

--- src/game/Miniboss.java
+++ src/game/Miniboss.java
@@ -3,12 +3,24 @@ package game;
 import edu.monash.fit2099.engine.IntrinsicWeapon;
 import edu.monash.fit2099.engine.Item;
 
+/**
+ * Class presenting the enemy Miniboss.
+ */
 public class Miniboss extends Enemy{
-
+	
+	/**
+	 * Super constructor of the Miniboss class extended from the Enemy class.
+	 * 
+	 * @param name name of the Miniboss
+	 */
 	public Miniboss(String name) {
 		super(name, 'M', Enemy.BASE_HITPOINTS / 2);
 	}
 	
+	/**
+	 * Overridden method from the parent class to modify enemy's default damage to be
+	 * half as such.
+	 */
 	@Override
 	protected IntrinsicWeapon getIntrinsicWeapon() {
 		int baseDamage = super.getIntrinsicWeapon().damage();
