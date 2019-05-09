0381c7353216cbaf2f35e04a8bdd504bc02fd695
Yashodha Liyanage Don <yliy0002@student.monash.edu>
2019-05-09 20:28:47 +1000

    Documented Grunt using Javadoc

--- src/game/Grunt.java
+++ src/game/Grunt.java
@@ -2,8 +2,19 @@ package game;
 
 import edu.monash.fit2099.engine.Actor;
 
+/**
+ * Class representing the enemy Grunt.
+ */
 public class Grunt extends Enemy {
 
+	/**
+	 * Super constructor of the Grunt extended from the Enemy class.
+	 * <P>
+	 * The method also adds an instance of FollowBehaviour to Grunt.
+	 * 
+	 * @param name name of the Grunt
+	 * @param player player
+	 */
 	// Grunts have 50 hitpoints and are always represented with a g
 	public Grunt(String name, Actor player) {
 		super(name, 'g');
