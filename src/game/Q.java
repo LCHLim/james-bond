2f208b7a21ba6180c8bb973335acdd51d70ff3d0
Yashodha Liyanage Don <yliy0002@student.monash.edu>
2019-05-10 19:42:48 +1000

    Documented Q using Javadoc

--- src/game/Q.java
+++ src/game/Q.java
@@ -2,13 +2,27 @@ package game;
 
 import edu.monash.fit2099.engine.*;
 
+/**
+ * Class representing the non-player character Q.
+ */
 public class Q extends NPC{
 
+	/**
+	 * Super constructor of the Q class extended from the NPC class.
+	 * <P>
+	 * The method also adds an instance of WanderBehaviour to Q.
+	 * 
+	 * @param name name of the non-player character
+	 */
 	public Q(String name) {
 		super(name, 'Q');
 		addBehaviour(new WanderBehaviour());
 	}
 
+	/**
+	 * Overridden method from parent class to let only Player to talk to and give plans
+	 * to Q.
+	 */
 	@Override
 	public Actions getAllowableActions(Actor otherActor, String direction, GameMap map) {
 		Actions actions = new Actions();
