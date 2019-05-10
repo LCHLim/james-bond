7aff16536fd92ece0e74aa60fd76e8952851aaa8
Yashodha Liyanage Don <yliy0002@student.monash.edu>
2019-05-10 19:12:26 +1000

    Added Javadoc documentation to Ninja

--- src/game/Ninja.java
+++ src/game/Ninja.java
@@ -4,16 +4,32 @@ import java.util.Random;
 
 import edu.monash.fit2099.engine.*;
 
+/**
+ * Class representing the enemy Ninja.
+ */
 public class Ninja extends Enemy{
 
 	Random rand = new Random();
 	
+	/**
+	 * Super constructor of the Ninja extended from the Enemy class.
+	 * <P>
+	 * The method also adds an instance of MoveAwayBehaviour and an instance
+	 * of ThrowPowderBehaviour to Ninja.
+	 * 
+	 * @param name
+	 * @param player
+	 */
 	public Ninja(String name, Actor player) {
 		super(name, 'N');
 		addBehaviour(new MoveAwayBehaviour(player));
 		addBehaviour(new ThrowPowderBehaviour(player));
 	}
 	
+	/**
+	 * Overridden method from parent class to change enemy's random action as a
+	 * non-player character.
+	 */
 	@Override
 	public Action playTurn(Actions actions, GameMap map, Display display) {
 		
