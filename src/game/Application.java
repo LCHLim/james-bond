5b3d060e1e69f85c0df7f2fff2754cd92c4a1688
Chin Hong Lim <clim0028@student.monash.edu>
2019-05-19 23:22:41 +1000

    Added rocket on moonbase. Player can now move between maps.

--- src/game/Application.java
+++ src/game/Application.java
@@ -8,6 +8,7 @@ import edu.monash.fit2099.engine.Display;
 import edu.monash.fit2099.engine.FancyGroundFactory;
 import edu.monash.fit2099.engine.GameMap;
 import edu.monash.fit2099.engine.Item;
+import edu.monash.fit2099.engine.Location;
 import edu.monash.fit2099.engine.MoveActorAction;
 import edu.monash.fit2099.engine.Player;
 import edu.monash.fit2099.engine.World;
@@ -59,40 +60,51 @@ public class Application {
 		GameMap moon = new GameMap(groundFactory, moonMap);
 		world.addMap(moon);
 		
+		Location earthRocketLocation = gameMap.at(18, 9);
+		Location moonRocketLocation = moon.at(9, 4);
+		
 		Item rocketToMoon = Item.newFurniture("Rocket", '^');
-		rocketToMoon.getAllowableActions().add(new MoveActorAction(moon.at(9, 4), "to Moon!"));
+		rocketToMoon.getAllowableActions().add(new MoveActorAction(moonRocketLocation, "to Moon!"));
+		
+		Item rocketToEarth = Item.newFurniture("Rocket", '^');
+		rocketToEarth.getAllowableActions().add(new MoveActorAction(earthRocketLocation, "to Earth!"));
 		
 		RocketPad rocketPad = new RocketPad(rocketToMoon);
-		gameMap.add(rocketPad, gameMap.at(18, 9));
+		gameMap.add(rocketPad, earthRocketLocation);
 		
+		moon.addItem(rocketToEarth, moonRocketLocation.x(), moonRocketLocation.y());
 		
-		
-		 
+	
 		Actor player = new StunnablePlayer("Player", '@', 1, 100);
 		world.addPlayer(player, gameMap, 2, 2);
 		
 		
-		Grunt grunt = new Grunt("Mongo", player);
-		gameMap.addActor(grunt, 0, 0);
-
-
-		Goon goon = new Goon("Norbert", player);
-		gameMap.addActor(goon, 10, 3);
-
-		Ninja ninja = new Ninja("Naruto", player);
-		gameMap.addActor(ninja, 10, 7);
-
+		Item rocketBody = new Item("Rocket Body", '[');
+		Item rocketEngine = new Item("Rocket Engine", '*');
+		gameMap.addItem(rocketEngine, 2, 3);
+		gameMap.addItem(rocketBody, 2, 4);
 		
-		 Q q = new Q("Q");
-		gameMap.addActor(q, 17, 6);
-		
-		Item rocketPlan = new Item("Rocket Plan", 'x');
-		gameMap.addItem(rocketPlan, 6, 2);
-		
-
-		Miniboss doctorMaybe = new Miniboss("Docter Maybe");
-		doctorMaybe.addItemToInventory(Item.newInventoryItem("Rocket Engine", '*'));
-		gameMap.addActor(doctorMaybe, 15, 2);
+//		Grunt grunt = new Grunt("Mongo", player);
+//		gameMap.addActor(grunt, 0, 0);
+//
+//
+//		Goon goon = new Goon("Norbert", player);
+//		gameMap.addActor(goon, 10, 3);
+//
+//		Ninja ninja = new Ninja("Naruto", player);
+//		gameMap.addActor(ninja, 10, 7);
+//
+//		
+//		 Q q = new Q("Q");
+//		gameMap.addActor(q, 17, 6);
+//		
+//		Item rocketPlan = new Item("Rocket Plan", 'x');
+//		gameMap.addItem(rocketPlan, 6, 2);
+//		
+//
+//		Miniboss doctorMaybe = new Miniboss("Docter Maybe");
+//		doctorMaybe.addItemToInventory(Item.newInventoryItem("Rocket Engine", '*'));
+//		gameMap.addActor(doctorMaybe, 15, 2);
 		
 		
 		world.run();
